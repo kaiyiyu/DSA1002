12df985d85f20532c7333543ebc778e77c35ffcd
Kai-Yu <kaiyu2871@gmail.com>
2021-07-06 21:55:01 +0800

    new: create and copy practical 1 files

---
+++ Practicals/P01/SortFilesPython/DSAsorts.py
@@ -0,0 +1,38 @@
+#
+# Data Structures and Algorithms COMP1002
+#
+# Python file to hold all sorting methods
+#
+
+def bubbleSort(A):
+    ...
+
+def insertionSort(A):
+    ...
+
+def selectionSort(A):
+    ...
+
+def mergeSort(A):
+    """ mergeSort - front-end for kick-starting the recursive algorithm
+    """
+    ...
+
+def mergeSortRecurse(A, leftIdx, rightIdx):
+    ...
+
+def merge(A, leftIdx, midIdx, rightIdx):
+    ...
+
+def quickSort(A):
+    """ quickSort - front-end for kick-starting the recursive algorithm
+    """
+    ...
+
+def quickSortRecurse(A, leftIdx, rightIdx):
+    ...
+
+def doPartitioning(A, leftIdx, rightIdx, pivotIdx):
+    ...
+
+
