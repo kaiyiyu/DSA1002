41f148dc49a9dae493617c45f4cc44def3cff900
Kai-Yu <kaiyu2871@gmail.com>
2021-07-06 22:52:00 +0800

    Add readFile function to sorting program

--- Practicals/P01/SortFilesPython/DSAsorts.py
+++ Practicals/P01/SortFilesPython/DSAsorts.py
@@ -4,6 +4,25 @@
 # Python file to hold all sorting methods
 #
 
+##### File Reading: One Line at a Time (from Lecture) #####
+
+def readFile(filename):
+    try:
+        fileObj = open(filename)
+
+        lineNum = 0
+        line = fileObj.readline()
+
+        while line:
+            lineNum += lineNum
+            processLine(line)
+            line = fileObj.readline()
+
+        fileObj.close()
+
+    except IOError as e:
+        print("Error in file processing: " + str(e))
+
 def bubbleSort(A):
     ...
 
@@ -11,28 +30,6 @@ def insertionSort(A):
     ...
 
 def selectionSort(A):
-    ...
-
-def mergeSort(A):
-    """ mergeSort - front-end for kick-starting the recursive algorithm
-    """
-    ...
-
-def mergeSortRecurse(A, leftIdx, rightIdx):
-    ...
-
-def merge(A, leftIdx, midIdx, rightIdx):
-    ...
-
-def quickSort(A):
-    """ quickSort - front-end for kick-starting the recursive algorithm
-    """
-    ...
-
-def quickSortRecurse(A, leftIdx, rightIdx):
-    ...
-
-def doPartitioning(A, leftIdx, rightIdx, pivotIdx):
     ...
 
 
